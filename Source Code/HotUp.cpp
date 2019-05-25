c71dce1523465bb9a7ad18f684c0cb3012a3ad87
Ezylryb <46624975+Ezylryb-BK@users.noreply.github.com>
2019-05-25 13:02:09 +0800

    v0.0.1

---
+++ Source Code/HotUp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    string r="";
+    bool flag=true;
+    cout<<"听 说 酱 紫 散 热 比 较 带 感 : "<<endl;
+    cout<<endl;
+    while (getline(cin,s)){
+        for(int i=0;i<s.length();i++){
+            r+=s[i];
+            if((s[i]&128)==0)
+            {
+                r+=" ~~~ ";
+            }else{
+                flag=!flag;
+                if(flag){
+                    r+=" ~~~ ";
+                }
+            }
+        }
+        cout<<endl;
+        cout<<endl;
+        printf("%s\n",r.c_str());
+        cout<<endl;
+        cout<<"------回车继续散热------";
+        r="";
+        getchar();
+        system("cls");
+        cout<<"听 说 酱 紫 散 热 比 较 带 感 : "<<endl;
+    }
+    return 0;
+}
+
